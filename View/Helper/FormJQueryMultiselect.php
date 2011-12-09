612c00b71114e177e5234ea12ae8bc85d050da71
Bert Hausmans <b.hausmans@bluetea.nl>
2011-12-09 09:39:57 +0000

    Renamed JQueryMultiSelect to JQueryMultiselect

--- View/Helper/FormJQueryMultiselect.php
+++ View/Helper/FormJQueryMultiselect.php
@@ -1,36 +1,28 @@
 <?php
-class HausDesign_View_Helper_FormJQueryMultiSelect extends Zend_View_Helper_FormSelect
+class HausDesign_View_Helper_FormJQueryMultiselect extends Zend_View_Helper_FormSelect
 {
-    public function formJQueryMultiSelect($name, $value = null, $attribs = null,
+    public function formJQueryMultiselect($name, $value = null, $attribs = null,
         $options = null, $listsep = "<br />\n")
     {
-        $this->view->headScript()->appendFile($this->view->baseUrl('/scripts/jquery-plugins/multiselect/src/jquery.multiselect.js', false));
-        $this->view->headLink()->appendStylesheet($this->view->baseUrl('/scripts/jquery-plugins/multiselect/jquery.multiselect.css', false), 'screen');
-
-        $params = array();
-        if (array_key_exists('jQueryParams', $attribs)) {
-            $params = $attribs['jQueryParams'];
-            unset($attribs['jQueryParams']);
+        if (array_key_exists('jqueryParams', $attribs)) {
+            $jqueryParams = $attribs['jqueryParams'];
+            $jqueryParams = ZendX_JQuery::encodeJson($jqueryParams);
+            unset($attribs['jqueryParams']);
+        } else {
+            $jqueryParams = array();
         }
 
-        $jquery = $this->view->jQuery();
-        $jquery->enable()
-               ->uiEnable();
-
-        // TODO: Allow translation of DatePicker Text Values to get this action from client to server
-        $params = ZendX_JQuery::encodeJson($params);
-
         $js = sprintf('%s("#%s").multiselect(%s);',
-                ZendX_JQuery_View_Helper_JQuery::getJQueryHandler(),
-                $attribs['id'],
-                $params
+            ZendX_JQuery_View_Helper_JQuery::getJQueryHandler(),
+            $attribs['id'],
+            $jqueryParams
         );
 
-        $jquery->addOnLoad($js);
+        $this->view->headScript()->appendFile($this->view->baseUrl('/scripts/jquery-plugins/multiselect/src/jquery.multiselect.js', false));
+        $this->view->headLink()->prependStylesheet($this->view->baseUrl('/scripts/jquery-plugins/multiselect/jquery.multiselect.css', false), 'screen');
+        
+        $this->view->jQuery()->enable()->uiEnable()->addOnLoad($js);
 
-        $xhtml = '';
-        $xhtml .= $this->formSelect($name, $value, $attribs, $options, $listsep);
-
-        return $xhtml;
+        return parent::formSelect($name, $value, $attribs, $options, $listsep);
     }
 }
